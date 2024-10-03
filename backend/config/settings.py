1e7a486c28bc84d705d6bc528ebdf2d5a2fba2e9
Jesús Cobacho Aguilera <jcobacho90@gmail.com>
2024-10-02 20:42:15 -0400

    [NEW] included CORS settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+j*kn#fx_9bvjxp4xmi_@68d-%^)ps86@vx_6dubwc95w0@8(z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 3rd party
+    'corsheaders',
+
     # my apps
     'api',
     'inventory'
@@ -46,6 +49,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -125,3 +129,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#NINJA
+NINJA_PAGINATION_PER_PAGE = 20
+
+#CORS
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
