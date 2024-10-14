bdd8799ea57bb56a91526e6d1223ba00986120e9
Jesús Cobacho Aguilera <jcobacho90@gmail.com>
2024-10-13 20:52:35 -0400

    [MOD] add page size to 15 items

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -160,7 +160,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 15
 }
 
 SIMPLE_JWT = {
