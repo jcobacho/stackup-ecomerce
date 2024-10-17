aa096fb5ee9a5b807986edafbe031023c754b6fe
Jesús Cobacho Aguilera <jcobacho90@gmail.com>
2024-10-17 03:06:50 -0400

    [IMP] include fields in order admin site

--- backend/sales/admin.py
+++ backend/sales/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from sales.models import Order
 
 # Register your models here.
+# admin.site.register(Order)
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["id", "client", "shipping_info", "total_amount", "paid"]
